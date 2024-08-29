72669a01883b41278dbafbdb589a8b9a2440af91
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-08-29 15:32:47 +0300

    add Problem 1

---
+++ problem1/script.js
@@ -0,0 +1,22 @@
+const sum_to_n_a = (n = 2) => {
+    let res = 0;
+
+    for (let i = 1; i <= n; i++) {
+        res += i;
+    }
+
+    return res;
+};
+
+const sum_to_n_b = (n = 2) => {
+    const numbersArr = Array.from({ length: n }, (_, index) => index + 1);
+    return numbersArr.reduce((a, b) => a + b);
+};
+
+const sum_to_n_c = (n = 2) => {
+    return (n * (n + 1)) / 2;
+};
+
+sum_to_n_a(5);
+sum_to_n_b(5);
+sum_to_n_c(4);
