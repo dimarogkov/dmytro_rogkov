50973d0d0d2ce6b60061b672e92ea3297d7f3b94
dima.rogkov <dima.rogkov@cheitgroup.com>
2024-09-20 16:06:02 +0300

    some fix

--- problem2/src/components/blocks/Currency.tsx
+++ problem2/src/components/blocks/Currency.tsx
@@ -4,10 +4,8 @@ type Props = {
 
 export const Currency: React.FC<Props> = ({ children }) => {
     return (
-        <section className='relative flex items-center justify-center w-full min-h-screen p-5'>
-            <div className='w-full max-w-[1000px] p-5 md:p-6 lg:p-8 xl:p-10 rounded-md border border-black'>
-                {children}
-            </div>
+        <section className='relative w-full max-w-[1000px] p-5 md:p-6 lg:p-8 xl:p-10 rounded-md border border-black'>
+            {children}
         </section>
     );
 };
